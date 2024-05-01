0959f618e068afe3a158d184bf698fe4d6deaafd
Husain Bhagat <hussainbhagat52@gmail.com>
2024-05-01 13:57:10 +0530

    Feat: Add the sign-up and email verification flow (partly)

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -1,8 +1,6 @@
 import { resend } from "@/lib/resend";
-
+import { ApiResponse } from '@/types/ApiResponse';
 import VerificationEmail from "../../emails/VerificationsEmail";
-
-import { ApiResponse } from "@/types/apiResponse";
 
 export async function sendVerificationEmail(
   email: string,
@@ -11,15 +9,14 @@ export async function sendVerificationEmail(
 ): Promise<ApiResponse> {
   try {
     await resend.emails.send({
-      from: "onboarding@resend.com",
+      from: 'onboarding@resend.dev',
       to: email,
-      subject: "Mystery Msg | Verification Code",
+      subject: 'Mystery Message Verification Code',
       react: VerificationEmail({ username, otp: verifyCode }),
     });
-
-    return { success: true, message: "Verification email sent successfully" };
+    return { success: true, message: 'Verification email sent successfully.' };
   } catch (emailError) {
-    console.error("Error sending verification email", emailError);
-    return { success: false, message: "Error sending verification email" };
+    console.error('Error sending verification email:', emailError);
+    return { success: false, message: 'Failed to send verification email.' };
   }
 }
